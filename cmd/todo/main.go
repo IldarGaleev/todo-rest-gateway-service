3a0629a4bc1ec9e24d1607fc5977659ef0b13641
Галеев Ильдар <ildar-galeev92@mail.ru>
2024-08-15 13:08:22 +0700

    refactoring: implement gRPCApplication

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,46 +1,16 @@
 package main
 
 import (
-	"context"
 	"log/slog"
 	"os"
+	"todoapiservice/internal/app/grpcapplication"
 	"todoapiservice/internal/app/httpapplication"
 	"todoapiservice/internal/http/handlers/authhandler"
 	"todoapiservice/internal/http/handlers/todoitemshandler"
 	"todoapiservice/internal/http/middlewares/jwtmiddleware"
 	"todoapiservice/internal/services/authprovider"
 	"todoapiservice/internal/services/todoprovider"
-
-	todoprotobufv1 "github.com/IldarGaleev/todo-backend-service/pkg/grpc/proto"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
-
-func unaryInterceptor(
-	ctx context.Context,
-	method string, req, reply any,
-	cc *grpc.ClientConn,
-	invoker grpc.UnaryInvoker,
-	opts ...grpc.CallOption,
-) error {
-
-	authCtx := metadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+"1234")
-	err := invoker(authCtx, method, req, reply, cc, opts...)
-
-	// If we got an unauthenticated response from the gRPC service, refresh the token
-	if status.Code(err) == codes.Unauthenticated {
-		// if err = jwt.refreshBearerToken(); err != nil {
-		//     return err
-		// }
-		updatedAuthCtx := metadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+"1234")
-		err = invoker(updatedAuthCtx, method, req, reply, cc, opts...)
-	}
-
-	return err
-}
 
 func main() {
 
@@ -53,28 +23,25 @@ func main() {
 		),
 	)
 
-	var opts []grpc.DialOption
+	gRPCApp := grpcapplication.New(
+		log,
+	)
 
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	opts = append(opts, grpc.WithUnaryInterceptor(unaryInterceptor))
-
-	conn, err := grpc.NewClient("localhost:9090", opts...)
+	client, err := gRPCApp.Start("localhost", 9090)
 
 	if err != nil {
 		panic(err)
 	}
 
 	defer func() {
-		err := conn.Close()
+		err := gRPCApp.Stop()
 		if err != nil {
 			panic(err)
 		}
 	}()
 
-	client := todoprotobufv1.NewToDoServiceClient(conn)
-
-	authProvider := authprovider.New(log, client)
-	todoProvider := todoprovider.New(log, client)
+	authProvider := authprovider.New(log, *client)
+	todoProvider := todoprovider.New(log, *client)
 
 	authHandle := authhandler.New(log, authProvider)
 	authMiddleware := jwtmiddleware.New(log, authProvider)
