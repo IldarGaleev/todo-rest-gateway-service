1615390080b106f9960bf1d081b9b7ff99fd529e
Галеев Ильдар <ildar-galeev92@mail.ru>
2024-08-18 16:30:25 +0700

    test: add auth provider Login method tests

--- internal/services/authprovider/provider_test.go
+++ internal/services/authprovider/provider_test.go
@@ -66,3 +66,70 @@ func TestAuthProvider_CheckSecret_ServerUnavailable(t *testing.T) {
 	require.ErrorIs(t, err, ErrAuthInternal)
 	require.Nil(t, user)
 }
+
+func TestAuthProvider_Login_Valid(t *testing.T) {
+	logger := slog.Default()
+	pr := mocks.New(false)
+	instance := New(logger, pr)
+	ctx := context.TODO()
+
+	user, err := instance.Login(ctx, "user1", "pass")
+
+	require.NoError(t, err)
+	require.Equal(t, "1:user1", *user.JWT)
+}
+
+func TestAuthProvider_Login_Invalid(t *testing.T) {
+	logger := slog.Default()
+	pr := mocks.New(false)
+	instance := New(logger, pr)
+	ctx := context.TODO()
+
+	testData := []struct {
+		name     string
+		eMail    string
+		password string
+	}{
+		{
+			name:     "Empty data",
+			eMail:    "",
+			password: "",
+		},
+		{
+			name:     "Empty username",
+			eMail:    "",
+			password: "pass",
+		},
+		{
+			name:     "Empty password",
+			eMail:    "user1",
+			password: "",
+		},
+		{
+			name:     "Invalid password",
+			eMail:    "user1",
+			password: "invalid",
+		},
+	}
+
+	for _, data := range testData {
+		t.Run(data.name, func(t *testing.T) {
+			user, err := instance.Login(ctx, data.eMail, data.password)
+
+			require.ErrorIs(t, err, ErrPermissionDenied)
+			require.Nil(t, user)
+		})
+	}
+}
+
+func TestAuthProvider_Login_ServerUnavailable(t *testing.T) {
+	logger := slog.Default()
+	pr := mocks.New(true)
+	instance := New(logger, pr)
+	ctx := context.TODO()
+
+	user, err := instance.Login(ctx, "user1", "pass")
+
+	require.ErrorIs(t, err, ErrAuthInternal)
+	require.Nil(t, user)
+}
